5602404696e2435738b2354187eaae86e1d57c79
iddiaz <correo@ivandiazdiaz.com>
2021-11-30 10:07:45 +0100

    gifsapp

---
+++ src/components/GifGrid.js
@@ -0,0 +1,34 @@
+// import React, {useState, useEffect } from 'react'
+import React from 'react'
+import { GifGridItem } from './GifGridItem';
+
+import { useFetchGifs } from './../hooks/useFetchGifs';
+
+export const GifGrid = ({category}) => {
+
+
+   const {data:images , loading } = useFetchGifs( category );
+
+
+
+
+   return (
+      <>
+         <h3> {category} </h3>
+         <div className="card-grid">
+
+            { loading && <p className="animate__animated animate__flash">Loading</p>  }
+            
+               {
+                  images.map(( img) => (
+                     <GifGridItem 
+                        key={img.id} 
+                        // img={img} 
+                        {...img}
+                        />
+                  ))
+               }            
+         </div>      
+      </>
+   )
+}
---
+++ src/helpers/getGifs.js
@@ -0,0 +1,18 @@
+
+export const getGifs = async( category ) => {
+   console.log('category??', category);
+   const url = `https://api.giphy.com/v1/gifs/search?limit=10&q=${ encodeURI(category) }&api_key=P88sYnaTcNSBWVQPiloP6uBwbLkJt4sg`;
+   const resp = await fetch( url );
+   const {data} = await resp.json();
+
+   const gifs = data.map( img => {
+      return {
+         id: img.id,
+         title: img.title,
+         url: img.images.downsized_medium.url
+      }
+   })
+
+   console.log(gifs);
+   return gifs;
+}
